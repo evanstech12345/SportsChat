7e1325769a64ee2bca8aecf35027f4bc6a04d6b1
evanstech12345 <evanshcode@gmail.com>
2021-08-17 00:48:32 -0400

    adding the routes and links for the different chat systems

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.use(express.static(path.join(__dirname, 'client/public')));
 app.get('/', (req, res) => {
   res.render('/opt/homebrew/Caskroom/miniforge/base/envs/sportstalk/client/view/index.ejs')
 })
+app.get('/MLB', (req, res) => {
+  res.render('/opt/homebrew/Caskroom/miniforge/base/envs/sportstalk/client/view/Mlb.ejs')
+})
+app.get('/NBA', (req, res) => {
+  res.render('/opt/homebrew/Caskroom/miniforge/base/envs/sportstalk/client/view/Nba.ejs')
+})
+app.get('/NFL', (req, res) => {
+  res.render('/opt/homebrew/Caskroom/miniforge/base/envs/sportstalk/client/view/Nfl.ejs')
+})
+app.get('/NHL', (req, res) => {
+  res.render('/opt/homebrew/Caskroom/miniforge/base/envs/sportstalk/client/view/Nhl.ejs')
+})
+
  
 
 app.listen(3000)
