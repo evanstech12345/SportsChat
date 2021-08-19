4d741f52dcd0b4c0c9f8c4f52943acff7f953d99
evanstech12345 <evanshcode@gmail.com>
2021-08-19 01:36:33 -0400

    updating links

--- server.js
+++ server.js
@@ -27,19 +27,19 @@ io.on('connection', (socket) => {
 
 
 //routes
-app.get('/', (req, res) => {
+app.get('sporttalk.netlify.app/', (req, res) => {
   res.render('/opt/homebrew/Caskroom/miniforge/base/envs/sportstalk/client/view/index.ejs')
 })
-app.get('/MLB', (req, res) => {
+app.get('sporttalk.netlify.app/MLB', (req, res) => {
   res.render('/opt/homebrew/Caskroom/miniforge/base/envs/sportstalk/client/view/Mlb.ejs')
 })
-app.get('/NBA', (req, res) => {
+app.get('sporttalk.netlify.app/NBA', (req, res) => {
   res.render('/opt/homebrew/Caskroom/miniforge/base/envs/sportstalk/client/view/Nba.ejs')
 })
-app.get('/NFL', (req, res) => {
+app.get('sporttalk.netlify.app/NFL', (req, res) => {
   res.render('/opt/homebrew/Caskroom/miniforge/base/envs/sportstalk/client/view/Nfl.ejs')
 })
-app.get('/NHL', (req, res) => {
+app.get('sporttalk.netlify.app/NHL', (req, res) => {
   res.render('/opt/homebrew/Caskroom/miniforge/base/envs/sportstalk/client/view/Nhl.ejs')
 })
 
